7dd6cc44a3de0cb8414c3c9727d7460e0a40b0e4
darlandesousa15 <darlandesousa15@gmail.com>
2023-02-09 07:13:27 -0300

    Add strings int and double

--- src/sintaxe_variaveis_e_fluxos/TestaVariaveis.java
+++ src/sintaxe_variaveis_e_fluxos/TestaVariaveis.java
@@ -10,5 +10,20 @@ public class TestaVariaveis {
 		idade = 37;
 		
 		System.out.println(idade);
+		
+		double salario;
+		salario = 200.20;
+		
+		System.out.println("Meu salario e de R$ " + salario);
+		
+		double divisao = 200.0 / 5.7;
+		// posso utilizar a declaração de variaveis dessa maneira
+		// ao utilizar o double e dividir se colocar um dos numeros
+		// que serão utilizados com o "." ele vai trazer o resultado
+		// em double e ñ em inteiro. Mas s ñ usar vai trazer o valor 
+		// como se fosse um int, dessa maneira dando errado o valor
+		// do resultado
+		System.out.println(divisao);
+		
 	}
 }
