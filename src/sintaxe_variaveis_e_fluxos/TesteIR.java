04d13235d714ac2a372d7acf1c6935ae8accf961
darlandesousa15 <darlandesousa15@gmail.com>
2023-02-11 07:22:31 -0300

    add class testa while

--- src/sintaxe_variaveis_e_fluxos/TesteIR.java
+++ src/sintaxe_variaveis_e_fluxos/TesteIR.java
@@ -2,22 +2,22 @@ package sintaxe_variaveis_e_fluxos;
 
 public class TesteIR {
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-        double salario = 2000.0;
-        double IRMenor = 120.0;
-        double IRMedio = 350.0;
-        double IRMaior = 636.0;
-        //ifs aqui
-        if (salario >= 1900.0 && salario <= 2801.0) {
-        	System.out.println("A sua aliquota é de 7.5%");
-        	System.out.println("O Imposto de Renda a ser pago é de R$ " + IRMenor);
-        } else if (salario >= 2801.01 && salario <= 3751.0 ) {
-        	System.out.println("A sua aliquota é de 15%");
-        	System.out.println("O Imposto de Renda a ser pago é de R$ " + IRMedio);
-        } else if (salario >= 3751.01 && salario <= 4664.0) {
-        	System.out.println("A sua aliquota é de 22,5%");
-        	System.out.println("O Imposto de Renda a ser pago é de R$ " + IRMaior);
-        }
-    }
+		double salario = 2000.0;
+		double IRMenor = 120.0;
+		double IRMedio = 350.0;
+		double IRMaior = 636.0;
+		// ifs aqui
+		if (salario >= 1900.0 && salario <= 2801.0) {
+			System.out.println("A sua aliquota é de 7.5%");
+			System.out.println("O Imposto de Renda a ser pago é de R$ " + IRMenor);
+		} else if (salario >= 2801.01 && salario <= 3751.0) {
+			System.out.println("A sua aliquota é de 15%");
+			System.out.println("O Imposto de Renda a ser pago é de R$ " + IRMedio);
+		} else if (salario >= 3751.01 && salario <= 4664.0) {
+			System.out.println("A sua aliquota é de 22,5%");
+			System.out.println("O Imposto de Renda a ser pago é de R$ " + IRMaior);
+		}
+	}
 }
---
+++ src/sintaxe_variaveis_e_fluxos/testaWhileJava.java
@@ -0,0 +1,16 @@
+package sintaxe_variaveis_e_fluxos;
+
+public class testaWhileJava {
+
+	public static void main(String[] args) {
+		
+		int contador = 0;
+		
+		while(contador <= 10) {
+			System.out.println(contador);
+			contador ++;
+		}
+		
+	}
+	
+}
