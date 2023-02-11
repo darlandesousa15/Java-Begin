21c602c2f7dffe91219890b49759ab2e6e8fc16c
darlandesousa15 <darlandesousa15@gmail.com>
2023-02-11 08:59:34 -0300

    Add testalaos 2

---
+++ src/sintaxe_variaveis_e_fluxos/testaLacos2.java
@@ -0,0 +1,19 @@
+package sintaxe_variaveis_e_fluxos;
+
+public class testaLacos2 {
+	
+	public static void main(String[] args) {
+		
+		for(int linha = 1; linha < 10; linha++) {
+			for(int coluna = 1; coluna <= 10; coluna++) {
+				if (coluna > linha) {
+					break;
+				}
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
+		
+
