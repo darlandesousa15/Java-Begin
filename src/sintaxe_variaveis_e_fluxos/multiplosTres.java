9932c408de2511a8c0790134a4375092885c639a
darlandesousa15 <darlandesousa15@gmail.com>
2023-02-11 20:07:56 -0300

    add potenciação

--- src/sintaxe_variaveis_e_fluxos/multiplosTres.java
+++ src/sintaxe_variaveis_e_fluxos/multiplosTres.java
@@ -6,6 +6,7 @@ public class multiplosTres {
 
 		for (int numero = 3; numero <= 100; numero++) {
 			if (numero % 3 == 0) {
+				// Divide por 3 e só vai mostrar aqueles que o resto seja 0
 				System.out.print(numero + "  ");
 			}
 		}
