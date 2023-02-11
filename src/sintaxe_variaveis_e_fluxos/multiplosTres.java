24127721fb651915d893b942a9ff55ecdbbbc285
darlandesousa15 <darlandesousa15@gmail.com>
2023-02-11 09:20:21 -0300

    Add nunbers that are multiples of three in class multiplosTres

---
+++ src/sintaxe_variaveis_e_fluxos/multiplosTres.java
@@ -0,0 +1,13 @@
+package sintaxe_variaveis_e_fluxos;
+
+public class multiplosTres {
+
+	public static void main(String[] args) {
+
+		for (int numero = 3; numero <= 100; numero++) {
+			if (numero % 3 == 0) {
+				System.out.print(numero + "  ");
+			}
+		}
+	}
+}
--- src/sintaxe_variaveis_e_fluxos/testaLacos2.java
+++ src/sintaxe_variaveis_e_fluxos/testaLacos2.java
@@ -5,7 +5,7 @@ public class testaLacos2 {
 	public static void main(String[] args) {
 		
 		for(int linha = 1; linha < 10; linha++) {
-			for(int coluna = 1; coluna <= 10; coluna++) {
+			for(int coluna = 1; coluna < 10; coluna++) {
 				if (coluna > linha) {
 					break;
 				}
